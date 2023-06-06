e790ef2e19d0e8e17f4b49cdde4b3fba00f3a413
Tshenolo Mos <tshenolo@gmail.com>
2023-06-06 10:54:54 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,24 @@
+import requests
+
+# Function: Generate response using ChatGPT
+def generate_response(message_content):
+    # Generate a response from the ChatGPT model
+    response = requests.post(
+        api_endpoint,
+        headers={
+            'Authorization': f'Bearer {api_key}',
+            'Content-Type': 'application/json'
+        },
+        json={
+            'messages': [
+                {'role': 'system', 'content': 'You are a user'},
+                {'role': 'user', 'content': message_content}
+            ]
+        }
+    )
+
+    # Parse the response and extract the generated message
+    response_data = response.json()
+    generated_message = response_data['choices'][0]['message']['content']
+
+    return generated_message
